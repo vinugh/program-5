dc4eea15b9fd6c4b8720c83368d1b0fd91b7b43b
vinugh <vinaygh2005@gmail.com>
2025-05-25 00:15:11 +0530

    save file

--- FibonacciSeries.java
+++ FibonacciSeries.java
@@ -1,19 +1,16 @@
-package prog5;
+FibonacciSeries.java
 public class FibonacciSeries {
     public static void main(String[] args) {
-        if (args.length == 5) {
-            System.out.println("eneter no.");
-            return;
-        }
+        int n = 20;
+        int a = 0, b = 1, c;
 
-        int n = Integer.parseInt(args[0]);
-        int a = 1, b = 1;
-        System.out.println("Fibonacci Series up to " + n + " terms:");
-        for (int i = 1; i <= n; i++) {
-            System.out.print(a + " ");
-            int next = a + b;
+        System.out.print("Fibonacci Series: " + a + ", " + b);
+
+        for (int i = 2; i < n; i++) {
+            c = a + b;
+            System.out.print(", " + c);
             a = b;
-            b = next;
+            b = c;
         }
     }
 }
